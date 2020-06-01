bde5ecd8e51451862ffd99588208f72d203b0f50
Ian Tsai <ianpaul32@gmail.com>
2020-06-02 03:18:24 +0800

    add comment  & add sheep personal web

--- personal_page/main.js
+++ personal_page/main.js
@@ -1,9 +1,13 @@
 
 function openInfo(person) {
-    console.log(person);
+    console.log(person);                // 使用 console.log 顯示一些資訊，可用來 debug
+
+    // 跳出 alert 範例 
     if( person === 'howard') {
-        alert('邪惡無敵!!')
+        alert('邪惡無敵!!')             
     }
+
+    // 修改 element 的文字內容
     else if (person === 'wan') {
         let node = document.getElementById('wan-title');
         let nickname = node.innerHTML === '阿針' ? '阿針遊戲王' : '阿針';
@@ -11,6 +15,8 @@ function openInfo(person) {
         node.style.color = node.style.color === 'red' ? 'blue' : 'red';
     
     }
+
+    // 新增 element 
     else if(person === 'jason') {
         let node = document.createElement('span');
         node.innerHTML = '🍕' ;
@@ -23,10 +29,13 @@ function openInfo(person) {
             document.getElementById("app").removeChild(node);
         }, 5000);
     }
+    
+    // 新增 class 做到小動畫的效果，並轉移畫面
     else if(person === 'sheep') {
         $('#Sheep > .selfie').addClass('active');
         setTimeout(function() {
             $('#Sheep > .selfie').removeClass('active');
+            window.location.href='https://biography.stackblitz.io';
         }, 1000)
     }
 }
